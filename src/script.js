fc883be0cb93e53258cb597f195bd43338f0db4c
bcavas <35956598+bcavas@users.noreply.github.com>
2021-08-03 10:10:07 +0800

    initial commit

---
+++ src/script.js
@@ -0,0 +1,15 @@
+/* Change Nav Color on Scroll */
+
+const nav = document.querySelector('#navbar');
+
+if (window.scrollY >= 10) {
+    nav.classList.add('scrolled')
+}
+
+window.onscroll = () => {
+  if (this.scrollY <= 10) {
+    nav.classList.remove('scrolled')
+  } else {
+    nav.classList.add('scrolled')
+  }
+};
